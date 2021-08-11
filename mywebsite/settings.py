69506c1435f1619211aa504bdda05f9b45d55781
scunden <slcunden@gmail.com>
2021-08-10 20:06:25 -0400

    static root update v2

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import sys
+if sys.argv[1] != 'runserver':
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
